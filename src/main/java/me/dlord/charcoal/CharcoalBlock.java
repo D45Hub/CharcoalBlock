99030c6233a0c5c581b0309fdde35f4ec9278485
D45Hub <61849425+D45Hub@users.noreply.github.com>
2020-12-25 12:51:59 +0100

    Restructured Repository

--- src/main/java/me/dlord/charcoal/CharcoalBlock.java
+++ src/main/java/me/dlord/charcoal/CharcoalBlock.java
@@ -1,23 +1,24 @@
 package me.dlord.charcoal;
 
 import net.minecraft.block.Block;
-import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
-import net.minecraftforge.common.ToolType;
+import net.minecraft.creativetab.CreativeTabs;
 
 
-/**
- * Class for the Charcoal Block which contains the block's properties.
- * 
- * @author D-Lord
- */
 public class CharcoalBlock extends Block
 {
 
-    public CharcoalBlock()
+    protected CharcoalBlock()
     {
-        super(Block.Properties.create(Material.IRON).hardnessAndResistance(5.0f, 6.0f)
-                .sound(SoundType.STONE).harvestLevel(1).harvestTool(ToolType.PICKAXE));
+        super(Material.rock);
+
+        setBlockName("charcoal_block");
+        setBlockTextureName("charcoal:charcoal_block");
+        setCreativeTab(CreativeTabs.tabBlock);
+        setHardness(5.0f);
+        setHarvestLevel("pickaxe", 0);
+        setResistance(6.0f);
+        setStepSound(soundTypeStone);
     }
 
 }
--- src/main/java/me/dlord/charcoal/CharcoalBlockMod.java
+++ src/main/java/me/dlord/charcoal/CharcoalBlockMod.java
@@ -1,22 +1,37 @@
 package me.dlord.charcoal;
 
-import net.minecraftforge.fml.common.Mod;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
 
 
-/**
- * Main class of this mod which starts the initialization of everything inside
- * this mod.
- * 
- * @author D-Lord
- */
-@Mod(CharcoalBlockMod.MODID)
+@Mod(modid = CharcoalBlockMod.MODID, version = CharcoalBlockMod.VERSION)
 public class CharcoalBlockMod
 {
     public static final String MODID = "charcoal";
+    public static final String VERSION = "1.0";
 
-    public CharcoalBlockMod()
+    public static CharcoalBlock charcoalBlock;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
     {
-        RegistryHandler.init();
+        charcoalBlock = new CharcoalBlock();
+
+        GameRegistry.registerBlock(charcoalBlock, charcoalBlock.getUnlocalizedName().substring(5));
+        GameRegistry.registerFuelHandler(new FuelHandler());
     }
 
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        GameRegistry.addRecipe(new ItemStack(charcoalBlock),
+                new Object[] {"CCC", "CCC", "CCC", 'C', new ItemStack(Items.coal, 1, 1)});
+        GameRegistry.addShapelessRecipe(new ItemStack(Items.coal, 9, 1),
+                new Object[] {charcoalBlock});
+    }
 }
